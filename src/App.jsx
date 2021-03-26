18e43fde6d4962add301f853d8a68ff1f5ba4944
unknown <hemakumarm72@gmail.com>
2021-03-26 20:22:02 +0530

    function.js upload files

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import './App.css';
 import React from 'react';
 
-function App() {
-  return (
+const Red = () => (
+  <div className="red">
+    <p>Red</p>
+  </div>
+);
 
-    <div className="hemakumar">
-      <p>hemakumar</p>
-    </div>
+const Blue = () => (
+  <div className="blue">
+    <p>Blue</p>
+  </div>
+);
 
-  );
-}
+const Green = () => (
+  <div className="green">
+    <p>green</p>
+  </div>
+);
 
+const App = () => (
+  <div className="app">
+
+    <Red />
+    <Blue />
+    <Green />
+
+  </div>
+);
 export default App;
